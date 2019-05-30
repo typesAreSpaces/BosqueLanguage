ef1e02f7de0af75d180f800dd69a3cb8692849b0
typesAreSpaces <jabelcastellanosjoo@unm.edu>
2019-05-30 09:24:45 -0700

    Reorganize files in verifer

--- ref_impl/src/verifier/collect_formulas.ts
+++ ref_impl/src/verifier/collect_formulas.ts
@@ -3,13 +3,7 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 
-import * as FS from "fs";
-import * as Path from "path";
-import chalk from "chalk";
-import { MIREmitter } from "../compiler/mir_emitter";
-import { PackageConfig, MIRAssembly, MIRFunctionDecl } from "../compiler/mir_assembly";
-import { MIRBody, MIRBasicBlock, MIROpTag, MIRBinCmp, MIRArgument, MIROp, MIRConstantArgument, MIRRegisterArgument, MIRVarLifetimeStart } from "../compiler/mir_ops";
-import { } from "../compiler/mir_ssa"; // We might need somethings from there, eventually!
+import { MIRBasicBlock, MIROpTag, MIRBinCmp, MIRArgument, MIROp, MIRRegisterArgument, MIRVarLifetimeStart, MIRVarStore, MIRReturnAssign, MIRJump, MIRJumpCond } from "../compiler/mir_ops";
 import { topologicalOrder, computeBlockLinks } from "../compiler/ir_info";
 import { TypeExpr, IntType, BoolType, StringType, UninterpretedType } from "../verifier/type_expr";
 import { VarExpr } from "../verifier/term_expr";
@@ -39,10 +33,10 @@ function resolveType(typeName: string): TypeExpr {
         case "NSCore::String": {
             return new StringType();
         }
-        case "true" : {
+        case "true": {
             return new BoolType();
         }
-        case "false" : {
+        case "false": {
             return new BoolType();
         }
         default: {
@@ -66,84 +60,22 @@ function resolveType(typeName: string): TypeExpr {
     }
 }
 
-function getControlFlow(app: string, section: string, fd: number): void {
-
-    let bosque_dir: string = Path.normalize(Path.join(__dirname, "../../"));
-
-    let files: { relativePath: string, contents: string }[] = [];
-    try {
-        const coredir = Path.join(bosque_dir, "src/core/core.bsq");
-        const coredata = FS.readFileSync(coredir).toString();
-
-        const collectionsdir = Path.join(bosque_dir, "src/core/collections.bsq");
-        const collectionsdata = FS.readFileSync(collectionsdir).toString();
-
-        const appdir = app;
-        const appdata = FS.readFileSync(appdir).toString();
-
-        files = [{ relativePath: coredir, contents: coredata }, { relativePath: collectionsdir, contents: collectionsdata }, { relativePath: appdir, contents: appdata }];
+function argumentToVarExpr(arg: MIRArgument, section: string): VarExpr {
+    if (arg instanceof MIRRegisterArgument) {
+        let argName = section + "__" + arg.nameID;
+        return new VarExpr(argName, resolveType(typesSeen.get(argName) as string));
     }
-    catch (ex) {
-        process.stdout.write(chalk.red(`Read failed with exception -- ${ex}\n`));
-        process.exit(1);
-    }
-
-    const { masm, errors } = MIREmitter.generateMASM(new PackageConfig(), files);
-
-    if (errors.length !== 0) {
-        for (let i = 0; i < errors.length; ++i) {
-            process.stdout.write(chalk.red(`Parse error -- ${errors[i]}\n`));
-        }
-        process.exit(1);
-    }
-
-    try {
-        const sectionName = section.split(":").join("_");
-        const invokeDecl = ((masm as MIRAssembly).functionDecls.get(section) as MIRFunctionDecl).invoke;
-        const ibody = (invokeDecl.body as MIRBody).body;
-        // If this is a FunctionDecl then the array of parameters,
-        // if it is not empty, will add more elements to the
-        // variable typesSeen
-        invokeDecl.params.map(x => typesSeen.set(sectionName + "__" + x.name, x.type.trkey));
-        if (typeof (ibody) === "string") {
-            process.stdout.write("Success as string!\n");
-            process.exit(0);
-        }
-        else {
-            // Here we generate the file.z3, essentially
-            // Since for Z3 is invalid a name with ":",
-            // we replace it with a "_"
-            collectFormulas(ibody, sectionName).toZ3(fd, false);
-            process.stdout.write("Success as blocks!\n");
-            process.exit(0);
-        }
-    }
-    catch (ex) {
-        process.stdout.write(chalk.red(`fail with exception -- ${ex}\n`));
-        process.exit(1);
-    }
-}
-
-function argumentToVarExpr(arg : MIRArgument, section : string) : VarExpr {
-    // Observation elements of type MIRConstantArgument
-    // start with "="
-    if(arg.nameID[0] === "="){
-        let argConstant = arg as MIRConstantArgument;
-        let argName = argConstant.stringify();
-        let result = new VarExpr(argName, resolveType(argConstant.nameID));
+    else {
+        let argName = arg.stringify();
+        let result = new VarExpr(argName, resolveType(arg.nameID));
         // With this we prevent printing constant argument
         // as declarations in Z3
         VarExpr.symbolTable.set(argName, true);
         return result;
     }
-    else{
-        let argRegister = arg as MIRRegisterArgument;
-        let argName = section + "__" + argRegister.nameID;
-        return new VarExpr(argName, resolveType(typesSeen.get(argName) as string));
-    }
 }
 
-function opToFormula(op: MIROp, section : string): FormulaExpr {
+function opToFormula(op: MIROp, section: string): FormulaExpr {
     switch (op.tag) {
         case MIROpTag.LoadConst:
         case MIROpTag.LoadConstTypedString:
@@ -255,10 +187,9 @@ function opToFormula(op: MIROp, section : string): FormulaExpr {
             return new PredicateExpr("notdone35", []);
         }
         case MIROpTag.MIRBinCmp: {
-
             let opBinCmp = op as MIRBinCmp;
             let opFormula = new PredicateExpr(opBinCmp.op, [
-                argumentToVarExpr(opBinCmp.lhs, section), 
+                argumentToVarExpr(opBinCmp.lhs, section),
                 argumentToVarExpr(opBinCmp.rhs, section)
             ]);
 
@@ -273,9 +204,19 @@ function opToFormula(op: MIROp, section : string): FormulaExpr {
             return new PredicateExpr("notdone38", []);
         }
         case MIROpTag.MIRVarStore: {
+            let opVarStore = op as MIRVarStore;
+            console.log("Implementing MIRVarStore--------------------------------------");
+            console.log(opVarStore);
+
+            // return new EqualityExpr(
+            //     argumentToVarExpr(opVarStore.src, section), 
+            //     argumentToVarExpr(opVarStore.name, section));
             return new PredicateExpr("notdone39", []);
         }
         case MIROpTag.MIRReturnAssign: {
+            let opReturnAssign = op as MIRReturnAssign;
+            console.log("Implementing MIRReturnAssign--------------------------------------");
+            console.log(opReturnAssign);
             return new PredicateExpr("notdone40", []);
         }
         case MIROpTag.MIRAssert: {
@@ -288,9 +229,15 @@ function opToFormula(op: MIROp, section : string): FormulaExpr {
             return new PredicateExpr("notdone43", []);
         }
         case MIROpTag.MIRJump: {
+            let opJump = op as MIRJump;
+            console.log("Implementing MIRJump--------------------------------------------------");
+            console.log(opJump);
             return new PredicateExpr("notdone44", []);
         }
         case MIROpTag.MIRJumpCond: {
+            let opJumpCond = op as MIRJumpCond;
+            console.log("Implementing MIRJumpCond----------------------------------------------");
+            console.log(opJumpCond);
             return new PredicateExpr("notdone45", []);
         }
         case MIROpTag.MIRJumpNone: {
@@ -337,17 +284,4 @@ function collectFormulas(ibody: Map<string, MIRBasicBlock>, section: string): Fo
         .reduce((a, b) => new AndExpr(a, b));
 }
 
-////
-//Entrypoint
-
-// Mac Machine
-// let bosqueFile = "/Users/joseabelcastellanosjoo/BosqueLanguage/ref_impl/src/test/apps/max/main.bsq"
-// Windows Machine
-let bosqueFile = "/Users/t-jocast/code/BosqueLanguage/ref_impl/src/test/apps/max/main4.bsq";
-
-let section = "NSMain::main";
-let fd = FS.openSync('file.z3', 'w');
-setImmediate(() => {
-    getControlFlow(bosqueFile, section, fd);
-    FS.closeSync(fd);
-});
+export { collectFormulas, typesSeen } 
---
+++ ref_impl/src/verifier/testing_collect_formulas.ts
@@ -0,0 +1,90 @@
+//-------------------------------------------------------------------------------------------------------
+// Copyright (C) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//-------------------------------------------------------------------------------------------------------
+
+import * as FS from "fs";
+import * as Path from "path";
+import chalk from "chalk";
+import { MIREmitter } from "../compiler/mir_emitter";
+import { PackageConfig, MIRAssembly, MIRFunctionDecl } from "../compiler/mir_assembly";
+import { MIRBody } from "../compiler/mir_ops";
+import { collectFormulas, typesSeen } from "./collect_formulas"
+
+function getControlFlow(app: string, section: string, fd: number): void {
+
+    let bosque_dir: string = Path.normalize(Path.join(__dirname, "../../"));
+
+    let files: { relativePath: string, contents: string }[] = [];
+    try {
+        const coredir = Path.join(bosque_dir, "src/core/core.bsq");
+        const coredata = FS.readFileSync(coredir).toString();
+
+        const collectionsdir = Path.join(bosque_dir, "src/core/collections.bsq");
+        const collectionsdata = FS.readFileSync(collectionsdir).toString();
+
+        const appdir = app;
+        const appdata = FS.readFileSync(appdir).toString();
+
+        files = [{ relativePath: coredir, contents: coredata }, { relativePath: collectionsdir, contents: collectionsdata }, { relativePath: appdir, contents: appdata }];
+    }
+    catch (ex) {
+        process.stdout.write(chalk.red(`Read failed with exception -- ${ex}\n`));
+        FS.closeSync(fd);
+        process.exit(1);
+    }
+
+    const { masm, errors } = MIREmitter.generateMASM(new PackageConfig(), files);
+
+    if (errors.length !== 0) {
+        for (let i = 0; i < errors.length; ++i) {
+            process.stdout.write(chalk.red(`Parse error -- ${errors[i]}\n`));
+        }
+        FS.closeSync(fd);
+        process.exit(1);
+    }
+
+    try {
+        const sectionName = section.split(":").join("_");
+        const invokeDecl = ((masm as MIRAssembly).functionDecls.get(section) as MIRFunctionDecl).invoke;
+        const ibody = (invokeDecl.body as MIRBody).body;
+        // If this is a FunctionDecl then the array of parameters,
+        // if it is not empty, will add more elements to the
+        // variable typesSeen
+        invokeDecl.params.map(x => typesSeen.set(sectionName + "__" + x.name, x.type.trkey));
+        if (typeof (ibody) === "string") {
+            process.stdout.write("Success as string!\n");
+            FS.closeSync(fd);
+            process.exit(0);
+        }
+        else {
+            // Here we generate the file.z3, essentially
+            // Since for Z3 is invalid a name with ":",
+            // we replace it with a "_"
+            collectFormulas(ibody, sectionName).toZ3(fd, false);
+            process.stdout.write("Success as blocks!\n");
+            FS.closeSync(fd);
+            process.exit(0);
+        }
+    }
+    catch (ex) {
+        process.stdout.write(chalk.red(`fail with exception -- ${ex}\n`));
+        FS.closeSync(fd);
+        process.exit(1);
+    }
+}
+
+////
+//Entrypoint
+
+setImmediate(() => {
+    // Mac Machine
+    let dirMachine = "/Users/joseabelcastellanosjoo/BosqueLanguage/ref_impl/src/test/apps"
+    // Windows Machine
+    // let dirMachine = "/Users/t-jocast/code/BosqueLanguage/ref_impl/src/test/apps";
+
+    let bosqueFile = dirMachine + "/max/main.bsq";
+    let section = "NSMain::max";
+    let fd = FS.openSync('file.z3', 'w');   
+    getControlFlow(bosqueFile, section, fd);
+});
