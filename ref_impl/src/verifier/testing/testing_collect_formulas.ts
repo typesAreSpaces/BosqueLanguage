a7e4de29dc6f6666bb542f1eb8a7dff545526893
typesAreSpaces <jabelcastellanosjoo@unm.edu>
2019-06-21 05:36:44 -0700

    I think I know what I have to do to implement the FunctionCall.

--- ref_impl/src/verifier/testing/testing_collect_formulas.ts
+++ ref_impl/src/verifier/testing/testing_collect_formulas.ts
@@ -6,7 +6,7 @@
 import * as FS from "fs";
 import { bosqueToIRBody } from "../util"
 import { collectFormula } from "../collect_formula";
-import { FormulaExpr } from "../formula_expr"
+// import { FormulaExpr } from "../formula_expr"
 
 setImmediate(() => {
     // Mac Machine
@@ -15,16 +15,15 @@ setImmediate(() => {
     // let directory = "/Users/t-jocast/code/BosqueLanguage/ref_impl/src/test/apps/max/";
     
     let fileName = "main.bsq";
-    let section = "NSMain::main";
+    let section = "NSMain::max3";
     
     let fd = FS.openSync("_" + (section.split(":").join("") + "_" + fileName).replace("bsq", "z3"), 'w');
     
     let [ir_body, sectionName] = bosqueToIRBody({directory: directory, fileName: fileName, section: section});
     let formula = collectFormula(ir_body, {directory: directory, fileName: fileName, section: sectionName});
-
-    FormulaExpr.initialDeclarationZ3(fd);
+    // FormulaExpr.initialDeclarationZ3(fd);
     formula.toZ3(fd);
-    FormulaExpr.checkSatZ3(fd);
+    // FormulaExpr.checkSatZ3(fd);
     
     FS.closeSync(fd);
 });
