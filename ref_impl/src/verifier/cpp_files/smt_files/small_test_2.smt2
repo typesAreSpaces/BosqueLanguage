931c463c036f66621a992ddd600429a2e99e1dfe
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2020-03-29 14:20:45 -0600

    Minor changes.

---
+++ ref_impl/src/verifier/cpp_files/smt_files/small_test_2.smt2
@@ -0,0 +1,11 @@
+(declare-fun x () Int)
+(declare-fun f (Int) Int)
+(declare-fun g (Int) Int)
+(declare-fun h (Int Int) Int)
+
+(assert (< (f 0) (- 1)))
+(assert (forall ((x Int) (y Int)) (! (and (>= (f x) 0) (= (h x y) (+ (g y) (g x)))) :pattern ((f x) (h x y) (g y) (g x)))))
+(assert (forall ((x Int)) (! (forall ((y Int)) (! (and (>= (f x) 0) (= (h x y) (+ (g y) (g x)))) :pattern ((h x y) (g y)))) :pattern ((f x) (g x)))))
+
+(check-sat)
+
--- ref_impl/src/verifier/cpp_files/src/main.cpp
+++ ref_impl/src/verifier/cpp_files/src/main.cpp
@@ -4,7 +4,8 @@
 int main(){
   z3::context ctx;
   try { 
-    GraphFormulas test("/home/jose/Documents/GithubProjects/BosqueLanguage/ref_impl/src/verifier/cpp_files/smt_files/small_test.smt2");
+    //GraphFormulas test("/home/jose/Documents/GithubProjects/BosqueLanguage/ref_impl/src/verifier/cpp_files/smt_files/small_test.smt2");
+    GraphFormulas test("/home/jose/Documents/GithubProjects/BosqueLanguage/ref_impl/src/verifier/cpp_files/smt_files/small_test_2.smt2");
     //GraphFormulas test("/home/jose/Documents/GithubProjects/BosqueLanguage/ref_impl/src/verifier/cpp_files/smt_files/test.smt2");
     std::cout << test << std::endl;
   }
