cc665b559cde3364be26a7929de9ca0e18a6b384
jose <jabelcastellanosjoo@unm.edu>
2020-07-23 01:12:00 -0600

    Minor changes.

--- impl/src/optimizer/run_optimizer.ts
+++ impl/src/optimizer/run_optimizer.ts
@@ -5,66 +5,72 @@
 
 import * as Path             from "path";
 import { isEqual }           from "lodash"
+import * as assert           from "assert";
 import { MIRInvokeKey, 
   MIRBasicBlock, 
   NormalizationContext }     from "./../compiler/mir_ops"
-import { MIRInvokeBodyDecl } from "./../compiler/mir_assembly"
+import { MIRInvokeBodyDecl,
+  MIRAssembly}               from "./../compiler/mir_assembly"
 import { bosqueToMASM }      from "./../verifier/typescript_files/util"
 
-class Optimizer {
+class MASMOptimizer {
 
-  ctx : NormalizationContext;
+  ctx  : NormalizationContext;
+  masm : MIRAssembly;
 
-  constructor(){
+  constructor(masm : MIRAssembly){
     this.ctx = new Map<string, number>();
+    this.masm = masm;
   }
 
-  current_way_to_hash(block : MIRBasicBlock) : object {
+  private normalizeMIRBasicBlock(block : MIRBasicBlock) : object {
     return block.ops.map(x => x.normalize(this.ctx));
   }
 
-  areEqualMIRBasicBlock(block1 : MIRBasicBlock, block2 : MIRBasicBlock) : boolean {
-    return isEqual(this.current_way_to_hash(block1), this.current_way_to_hash(block2));
+  private areEqualMIRBasicBlock(block1 : MIRBasicBlock, block2 : MIRBasicBlock) : boolean {
+    return isEqual(this.normalizeMIRBasicBlock(block1), this.normalizeMIRBasicBlock(block2));
   }
 
-  areEqualMapMIRBasicBlock(map_b1 : Map<string, MIRBasicBlock>, map_b2 : Map<string, MIRBasicBlock>) : boolean {
-    for(let [f_name, basic_block] of map_b1){
-      const entry_second_map = map_b2.get(f_name);
-      if(entry_second_map == undefined)
+  private areEqualMapMIRBasicBlock(map_b1 : Map<string, MIRBasicBlock>, map_b2 : Map<string, MIRBasicBlock>) : boolean {
+    for(let [f_name, entry_b1] of map_b1){
+      const entry_b2 = map_b2.get(f_name);
+      if(entry_b2 == undefined)
         return false;
       else{
-        if(!this.areEqualMIRBasicBlock(basic_block, entry_second_map))
+        if(!this.areEqualMIRBasicBlock(entry_b1, entry_b2))
           return false;
       }
     }
     return true;
   }
 
-  functionDeclsToRemove(invokeDecls : Map<MIRInvokeKey, MIRInvokeBodyDecl> ) 
+  functionDeclsToRemove() 
     : Map<MIRInvokeKey, MIRInvokeKey> {
+
       const repr_funcs = new Set<MIRInvokeKey>();
       const to_remove  = new Map<MIRInvokeKey, MIRInvokeKey>();
 
-      for(let [function_name, function_body_decl] of invokeDecls){
+      for(let [function_name, function_body_decl] of this.masm.invokeDecls){
         let found = false;
         for(let repr_func of repr_funcs){
+          const newest_block = function_body_decl.body.body;
           // The following cannot be undefined, by construction
-          const block = (invokeDecls.get(repr_func) as MIRInvokeBodyDecl).body.body;
-          if(this.areEqualMapMIRBasicBlock(function_body_decl.body.body, block)){
+          const previous_body_decl = this.masm.invokeDecls.get(repr_func) as MIRInvokeBodyDecl;
+          assert(previous_body_decl instanceof MIRInvokeBodyDecl);
+          const previous_block = previous_body_decl.body.body;
+          if(this.areEqualMapMIRBasicBlock(newest_block, previous_block)){
             to_remove.set(function_name, repr_func);
             found = true;
             break;
           }
         }
-        if(found == false){
+        if(found == false)
           repr_funcs.add(function_name);
-        }
       }
-
       return to_remove;
     }
 
-  normalizeMapMIRBasicBlock(input : Map<string, MIRBasicBlock>) : Map<string , object> {
+  private normalizeMapMIRBasicBlock(input : Map<string, MIRBasicBlock>) : Map<string , object> {
     const result = new Map<string, object>();
     input.forEach((value, key) => {
       result.set(key, value.ops.map(x => x.normalize(this.ctx)))
@@ -72,7 +78,7 @@ class Optimizer {
     return result;
   }
 
-  areEqualMapNormalizedMIRBasicBlock(map_b1 : Map<string, object>, map_b2 : Map<string, object>) : boolean {
+  private areEqualMapNormalizedMIRBasicBlock(map_b1 : Map<string, object>, map_b2 : Map<string, object>) : boolean {
     for(let [f_name, basic_block] of map_b1){
       const entry_second_map = map_b2.get(f_name);
       if(entry_second_map == undefined)
@@ -85,14 +91,14 @@ class Optimizer {
     return true;
   }
 
-  functionDeclsToRemove2(invokeDecls : Map<MIRInvokeKey, MIRInvokeBodyDecl> ) 
+  functionDeclsToRemove2() 
     : Map<MIRInvokeKey, MIRInvokeKey> {
       const repr_funcs = new Set<MIRInvokeKey>();
       const to_remove  = new Map<MIRInvokeKey, MIRInvokeKey>();
 
       const normalized_invokeDecls = new Map<MIRInvokeKey, Map<string, object> >();
 
-      invokeDecls.forEach((value, key) => {
+      this.masm.invokeDecls.forEach((value, key) => {
         normalized_invokeDecls.set(key, this.normalizeMapMIRBasicBlock(value.body.body));
       });
 
@@ -107,14 +113,12 @@ class Optimizer {
             break;
           }
         }
-        if(found == false){
+        if(found == false)
           repr_funcs.add(function_name);
-        }
       }
 
       return to_remove;
     }
-
 }
 
 setImmediate(() => {
@@ -125,8 +129,10 @@ setImmediate(() => {
   const masm = bosqueToMASM([`${file_directory}/${file_name}`], "cpp"); 
 
   if(masm !== undefined){
-    //console.log(new Optimizer().functionDeclsToRemove(masm.invokeDecls));
-    console.log(new Optimizer().functionDeclsToRemove2(masm.invokeDecls));
+    console.log(new MASMOptimizer(masm).functionDeclsToRemove());
+    //console.log(new MASMOptimizer(masm).functionDeclsToRemove2());
+
+    console.log(masm);
 
     process.stdout.write(`Done!\n`);
     process.exit(0);
