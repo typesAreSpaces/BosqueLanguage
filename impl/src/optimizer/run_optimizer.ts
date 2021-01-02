4b5bc23a9996303b5a3104d9ed81a1e82a7926fa
jose <jabelcastellanosjoo@unm.edu>
2021-01-02 02:36:50 -0700

    Fixing previous fstar implementation.

--- impl/src/optimizer/run_optimizer.ts
+++ impl/src/optimizer/run_optimizer.ts
@@ -3,23 +3,20 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 
-import * as Path             from "path";
+import * as Commander from "commander";
 import { bosqueToMASM }      from "./../verifier/typescript_files/util"
 import { MASMOptimizer }     from "./masm_optimizer"
   
+Commander.parse(process.argv);
+
 setImmediate(() => {
 
-  const file_directory = Path.normalize(Path.join(__dirname, "./../../src/test/tests/optimizer"));
-  const file_name = "simple_test.bsq";
-
-  const masm = bosqueToMASM([`${file_directory}/${file_name}`], "cpp"); 
+  const masm = bosqueToMASM(Commander.args, "cpp"); 
 
   if(masm !== undefined){
     const to_remove = new MASMOptimizer(masm).functionDeclsToRemove();
     console.log(to_remove);
-    //console.log(new MASMOptimizer(masm).functionDeclsToRemove2());
     masm.simplify(to_remove);
-    //console.log(masm.invokeDecls);
 
     process.stdout.write(`Done!\n`);
     process.exit(0);
--- impl/src/verifier/typescript_files/run_verifier.ts
+++ impl/src/verifier/typescript_files/run_verifier.ts
@@ -3,21 +3,23 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 
-import * as Path from "path";
+import * as Commander from "commander";
 import { bosqueToMASM } from "./util"
 import { TranslatorBosqueFStar } from "./translator_bosque_fstar";
 import { MASMOptimizer  } from "../../optimizer/masm_optimizer"
 
+Commander
+  .option("-e --entrypoint [entrypoint]", "Entrypoint to symbolically test", "NSMain::main")
+Commander.parse(process.argv);
+
 setImmediate(() => {
 
-  const file_directory = Path.normalize(Path.join(__dirname, "./../../../src/test/apps/max"));
-  //const file_name = "simple_main.bsq";
-  const file_name = "main.bsq";
-  const fkey = "NSMain::main";
+  const first_file_name = Commander.args[0];
+  const file_name = first_file_name.substring(first_file_name.lastIndexOf('/') + 1);
 
-  const masm = bosqueToMASM([`${file_directory}/${file_name}`], "cpp"); 
+  const masm = bosqueToMASM(Commander.args, "cpp"); 
 
-  const fstar_file_name = (fkey + "_" + file_name)
+  const fstar_file_name = (Commander.entrypoint + "_" + file_name)
     .replace(new RegExp("#", 'g'), "_")
     .replace(new RegExp("\\$", 'g'), "_")
     .replace(new RegExp(":", 'g'), "_")
@@ -26,19 +28,11 @@ setImmediate(() => {
   if(masm !== undefined){
 
     const to_remove = new MASMOptimizer(masm).functionDeclsToRemove();
-    //masm.invokeDecls.forEach((value, key) => {
-      //console.log("----New entry");
-      //console.log(key);
-      //console.log(value);
-      //console.log(value.body)
-    //});
-    //console.log("------ To remove");
-    //console.log(to_remove);
     masm.simplify(to_remove);
 
     const translator = new TranslatorBosqueFStar(masm, fstar_file_name);
 
-    translator.generateFStarCode(fkey); 
+    translator.generateFStarCode(Commander.entrypoint); 
     //translator.runFStarCode(20, 15, 5);
 
     process.stdout.write(`Done!\n`);
